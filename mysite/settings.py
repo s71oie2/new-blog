b57dbf060b18d66aa572f748a642f3e28e0298a6
soie_kim <s71oie2@gmail.com>
2018-10-08 11:45:42 +0900

    Changed ...

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookmark.apps.BookmarkConfig',     # bookmark 앱을 추가 등록
+    'blog.apps.BlogConfig',    # blog 앱을 추가 등록
 ]
 
 MIDDLEWARE = [
